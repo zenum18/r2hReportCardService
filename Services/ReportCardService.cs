501a3b8a1e601bcfbf3d871a657ea7974cfde145
Lynnette <lmunez@redventures.com>
2018-08-15 22:47:34 -0400

    added ReportCardService File

---
+++ Services/ReportCardService.cs
@@ -0,0 +1,12 @@
+using R2hReportCardService.Models;
+
+namespace R2hReportCardService.Services
+{
+  public class ReportCardService
+  {
+    public void createMentorReportCard(MentorReportCard requestBody)
+    {
+      
+    }
+  }
+}
